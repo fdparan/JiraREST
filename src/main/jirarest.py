636450f73f2841b1326a62fa6bf6619a590e3389
JiraREST <francisparan@gmail.com>
2019-03-21 12:36:21 -0400

    added ability to get only a subset of keys from get_projects

--- src/main/jirarest.py
+++ src/main/jirarest.py
@@ -7,25 +7,28 @@ class Server:
         self.context = "/rest/api/latest" if use_latest else "/rest/api/2"
 
         self.base = base_url
-        self._uri()
-
         self.auth_token = requests.auth.HTTPBasicAuth(user, password) if user and password else None
 
-    def _uri(self):
-        self.uri = self.base + self.context
+    def uri(self):
+        return self.base + self.context
 
     def change_base_url(self, new_base_url):
         self.base = new_base_url
-        self._uri()
 
     def get_operation(self, resource, verbose=False):
-        return requests.get(self._rest_call(resource, verbose), auth=self.auth_token).json()
+        return requests.get(self.generate_rest(resource, verbose), auth=self.auth_token).json()
 
-    def _rest_call(self, resource, verbose=False):
-        call = self.uri + resource
+    def generate_rest(self, resource, verbose=False):
+        rest_uri = self.uri() + resource
         if verbose:
-            print(call)
-        return call
+            print(rest_uri)
+        return rest_uri
 
-    def get_projects(self):
-        return self.get_operation("/project")
+    def get_projects(self, get_keys=None):
+
+        projects = self.get_operation("/project")
+
+        if get_keys:
+            projects = list(map(lambda x: dict((k, x[k] if k in x.keys() else None) for k in get_keys), projects))
+
+        return projects
--- src/tests/test_jirarest.py
+++ src/tests/test_jirarest.py
@@ -6,58 +6,55 @@ base = "http://localhost:8080"
 
 
 class MyTestCase(unittest.TestCase):
+
+    def test_ServerObject(self):
+        server = jirarest.Server(base)
+
+        self.assertIsNotNone(server)
+
     def test_ServerURI(self):
         server = jirarest.Server(base)
 
-        self.assertIsNotNone(server.uri)
-        self.assertEqual(server.uri, base + "/rest/api/2")
+        self.assertIsNotNone(server.uri())
+        self.assertEqual(server.uri(), base + "/rest/api/2")
 
     def test_ServerLatestURI(self):
         server = jirarest.Server(base, use_latest=True)
-        self.assertEqual(server.uri, base + "/rest/api/latest")
-
-    def test__uri(self):
-        server = jirarest.Server(base)
-        server._uri()
-
-        self.assertEqual(server.uri, base + "/rest/api/2")
+        self.assertEqual(server.uri(), base + "/rest/api/latest")
 
     def test_ServerChangeBaseURL(self):
         server = jirarest.Server(base)
-
-        self.assertEqual(server.uri, base + "/rest/api/2")
+        self.assertEqual(server.uri(), base + "/rest/api/2")
 
         new_base = "https://jira.atlassian.com"
-
         server.change_base_url(new_base)
-
-        self.assertEqual(server.uri, new_base + "/rest/api/2")
+        self.assertEqual(server.uri(), new_base + "/rest/api/2")
 
     @mock.patch('builtins.print')
-    def test__rest_call(self, mock_print):
+    def test_generate_rest(self, mock_print):
         resource = "/project"
         server = jirarest.Server(base, 'fparan', 'password')
 
-        test = server._rest_call(resource)
+        test = server.generate_rest(resource)
 
-        self.assertEqual(test, server.uri + resource)
+        self.assertEqual(test, server.uri() + resource)
         self.assertFalse(mock_print.called)
 
-        test = server._rest_call('/issues', verbose=True)
+        test = server.generate_rest('/issues', verbose=True)
 
-        self.assertNotEqual(test, server.uri + resource)
-        self.assertEqual(test, server.uri + '/issues')
+        self.assertNotEqual(test, server.uri() + resource)
+        self.assertEqual(test, server.uri() + '/issues')
         self.assertTrue(mock_print.called)
 
-    @mock.patch('src.main.jirarest.Server._rest_call')
+    @mock.patch('src.main.jirarest.Server.generate_rest')
     @mock.patch('src.main.jirarest.requests.get')
-    def test_ServerGETOperation(self, mock_get, mock__rest_call):
+    def test_ServerGETOperation(self, mock_get, mock_generate_rest):
         get_resource = "/project"
         server = jirarest.Server(base, 'fparan', 'password')
 
         server.get_operation(get_resource)
         self.assertTrue(mock_get.called)
-        self.assertTrue(mock__rest_call.called)
+        self.assertTrue(mock_generate_rest.called)
 
     @mock.patch('src.main.jirarest.Server.get_operation')
     def test_ServerGETProjects(self, mock_get_operation):
@@ -65,6 +62,31 @@ class MyTestCase(unittest.TestCase):
         server.get_projects()
 
         self.assertTrue(mock_get_operation.called)
+
+    @mock.patch('src.main.jirarest.Server.get_operation')
+    def test_ServerGETProjectsKeyValue(self, mock_get_operation):
+        server = jirarest.Server(base)
+        mock_get_operation.return_value = [
+            {"name": "foo",  'key': "FOO",  "id": "123",  'expand': 'description,lead,url,projectKeys'},
+            {"name": "bar",  'key': "BAR",  "id": "456",  'expand': 'description,lead,url,projectKeys'},
+            {"name": "blah", 'key': "BLAH", "id": "789",  'expand': 'description,lead,url,projectKeys'}]
+
+        result = server.get_projects(get_keys=['name','id'])
+        expected = [
+            {"name": "foo",  "id": "123"},
+            {"name": "bar",  "id": "456"},
+            {"name": "blah", "id": "789"}]
+
+        self.assertTrue(mock_get_operation.called)
+        self.assertEqual(result, expected)
+
+        result = server.get_projects(get_keys=['name'])
+        expected = [
+            {"name": "foo"},
+            {"name": "bar"},
+            {"name": "blah"}]
+
+        self.assertEqual(result, expected)
 
 if __name__ == '__main__':
     unittest.main()
